5cd32d52c7b7303c54f28120bdecae4c319cd9bf
TroubledKezoo1 <95494380+TroubledKezoo1@users.noreply.github.com>
2024-04-07 05:19:06 +0300

    Update 0-putchar.c
    ft_putchar

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
 
+int ft_putchar(int c);//if write function can't write the argument to terminal screen the ft_putchar function returns -1;
+
+int ft_putchar(int c)
+{
+  return(write(1,&c,1)); 
+}
+
 int main(void)
 {
-
-printf("_putchar\n");
-
-return (0);
+  int i;
+  char *str;
+  
+  i = 0;
+  str = "_putchar\n";
+  while(str[i])
+    if(ft_putchar(str[i++]) == -1)
+      return(1);
+  return (0);
 }
